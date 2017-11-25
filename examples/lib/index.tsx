1eb6b29fb66f19659d1f142d65c7e87f1a0190e6
woolyang <249877448@qq.com>
2017-11-25 23:27:27 +0800

    button-theme

--- examples/lib/index.tsx
+++ examples/lib/index.tsx
@@ -9,6 +9,10 @@ class Example extends React.Component {
             <div>
                 <Button>默认按钮</Button>
                 <Button type="primary" >主要按钮</Button>
+                <Button type="success" >成功按钮</Button>
+                <Button type="warning" >警告按钮</Button>
+                <Button type="danger" >危险按钮</Button>
+                <Button type="info" >信息按钮</Button>
                 <Hello compiler="TypeScript" framework="React" />
             </div>
         )
