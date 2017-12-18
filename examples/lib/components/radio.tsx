b28c204b561419693a187a84736c38b5bc63acf5
woolyang <249877448@qq.com>
2017-12-18 12:24:11 +0800

    radio

--- examples/lib/components/radio.tsx
+++ examples/lib/components/radio.tsx
@@ -2,11 +2,29 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Hello, Radio } from '../../../src/components/index';
 
-export class RadioDemo extends React.Component {
+export interface RadioDemoProps {
+    value?: string | number | boolean;
+}
+export class RadioDemo extends React.Component<RadioDemoProps, any> {
+
+    constructor(props: Object) {
+        super(props);
+
+        this.state = {
+            value: 1
+        }
+    }
+
+    onChange(value: any) {
+        this.setState({ value });
+    }
+
     render() {
+        const { value } = this.state;
         return (
             <div style={{ width: '200px', margin: '0 auto' }} >
-                <Radio>123</Radio>
+                <Radio value="1" checked={this.state.value == 1} onChange={this.onChange.bind(this)} >选项1</Radio>
+                <Radio value="2" checked={this.state.value == 2} onChange={this.onChange.bind(this)} >选项2</Radio>
             </div>
         )
     }
--- src/components/radio/radio.tsx
+++ src/components/radio/radio.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import * as classNames from 'classnames';
 import './style/radio.less';
 
 export interface RadioProps {
@@ -7,6 +8,7 @@ export interface RadioProps {
     checked?: boolean;
     value?: string | number | boolean;
     disabled?: boolean;
+    onChange?: any;
 }
 
 export class Radio extends React.Component<RadioProps, any> {
@@ -21,29 +23,67 @@ export class Radio extends React.Component<RadioProps, any> {
         checked: PropTypes.bool,
         value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]),
         disabled: PropTypes.bool,
+        onChange: PropTypes.func,
     };
+
+    constructor(props: Object) {
+        super(props);
+
+        this.state = {
+            checked: this.getChecked(props)
+        };
+    }
+
+    componentWillReceiveProps(nextProps: Object) {
+        const checked = this.getChecked(nextProps);
+
+        if (this.state.checked != checked) {
+            this.setState({ checked });
+        }
+    }
+
+    getChecked(props: RadioProps): boolean {
+        return Boolean(props.checked)
+    }
+
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const checked = e.target.checked;
+
+        const { onChange, value } = this.props;
+        if (onChange && checked) {
+            onChange(value);
+        }
+        this.setState({ checked });
+    }
 
 
     render() {
 
         const {
             prefixCls,
-            checked,
             disabled,
             value,
             children
         } = this.props;
 
+        const { checked } = this.state;
+
+        const classes = classNames(
+            `${prefixCls}-wrapper`,
+            checked && `${prefixCls}-checked`
+        )
+
         return (
-            <label>
-                <span>
+            <label className={`${prefixCls}`} >
+                <span className={classes}>
                     <input
                         type="radio"
                         className={`${prefixCls}-input`}
                         checked={checked}
                         disabled={disabled}
+                        onChange={this.handleChange}
                     />
-                    <span></span>
+                    <span className={`${prefixCls}-inner`} ></span>
                 </span>
                 <span>{children || value}</span>
             </label>
