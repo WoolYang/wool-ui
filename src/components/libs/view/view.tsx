e4a3a53dcad383fee563548d6ba447a2347abc80
woolyang <249877448@qq.com>
2017-12-27 22:14:42 +0800

    view

---
+++ src/components/libs/view/view.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+
+export interface ViewProps {
+    show?: boolean;
+    children?: any;
+}
+
+export default class View extends React.Component<ViewProps, any> {
+    render() {
+        const style = this.props.hasOwnProperty('show') && !this.props.show && {
+            display: 'none'
+        };
+
+        return React.cloneElement(React.Children.only(this.props.children), {
+            style: Object.assign({}, this.props.children.props.style, style)
+        });
+    }
+}
