ca75b93c79ef84b672eaeb4a24dc71d4ae6fa7f3
woolyang <249877448@qq.com>
2018-04-14 16:21:28 +0800

    table

---
+++ src/components/table/table.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './style/table.less';
+
+export interface TableProps { compiler: string; framework: string; }
+
+export class Table extends React.Component<TableProps, any> {
+    render() {
+        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
